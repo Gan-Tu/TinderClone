e11a06d17d05c1182b991fcd75f293feb649d344
Gan Tu <github@tugan.dev>
2023-09-04 18:32:45 -0400

    Initial Commit

---
+++ TinderClone/TinderCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderCloneApp.swift
+//  TinderClone
+//
+//  Created by Gan Tu on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct TinderCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
