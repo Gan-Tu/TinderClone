02e398161ca0d0cb894b3488f649ab6126e5882b
Gan Tu <github@tugan.dev>
2023-10-15 17:24:54 -0400

    feat: implement card image scroller

---
+++ TinderClone/Home/CardViews/CardImageScroller.swift
@@ -0,0 +1,111 @@
+//
+//  CardImageScroller.swift
+//  TinderClone
+//
+//  Created by Gan Tu on 10/15/23.
+//
+
+import SwiftUI
+import KingfisherSwiftUI
+
+struct CardImageScroller: View {
+    var person: Person
+    @Binding var fullScreenMode: Bool
+    
+    @State private var imageIndex = 0
+    
+    func swipeRight() {
+        imageIndex = min(imageIndex + 1, person.imageURLS.count-1)
+    }
+    
+    func swipeLeft() {
+        imageIndex = max(imageIndex - 1, 0)
+    }
+    
+    var body: some View {
+        GeometryReader { geo in
+            ZStack {
+                ZStack {
+                    KFImage(person.imageURLS[imageIndex])
+                        .placeholder {
+                            Color.white
+                        }
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: geo.size.width, height: geo.size.height)
+                        .clipped()
+                        .onTapGesture(perform: {
+                            swipeLeft();
+                        })
+                    
+                    HStack {
+                        Rectangle()
+                            .onTapGesture {
+                                swipeLeft()
+                            }
+                        
+                        Rectangle()
+                            .onTapGesture {
+                                swipeRight()
+                            }
+                    }
+                    .foregroundColor(Color.white.opacity(0.01))
+                } // ZSTACK
+                
+                VStack {
+                    HStack {
+                        ForEach(0..<person.imageURLS.count, id:\.self) { imageIndex in
+                            RoundedRectangle(cornerRadius: 20)
+                                .frame(height: 4)
+                                .foregroundColor(self.imageIndex == imageIndex ? Color.white : Color.gray.opacity(0.5))
+                        }
+                    }
+                    .padding(.top, 10)
+                    .padding(.horizontal, fullScreenMode ? 0 : 12)
+                    
+                    Spacer()
+                    
+                    if !fullScreenMode {
+                        HStack {
+                            VStack(alignment:. leading) {
+                                HStack {
+                                    Text(person.name)
+                                        .font(.system(size: 32, weight: .heavy))
+                                    
+                                    Text("\(person.age)")
+                                        .font(.system(size: 28, weight: .light))
+                                }
+                                
+                                Text(person.bio)
+                                    .font(.system(size: 18, weight: .medium))
+                                    .lineLimit(2)
+                            } // VSTACK
+                            
+                            Spacer()
+                            
+                            Button(action: {
+                                // TODO
+                            }, label: {
+                                Image(systemName: "info.circle.fill")
+                                    .font(.system(size: 26, weight: .medium))
+                            })
+                        } // HSTACK
+                        .padding(16)
+                    }
+                }
+                .foregroundColor(Color.white)
+                
+            } // ZSTACK
+            .cornerRadius(6)
+            .shadow(radius: 5)
+        }
+    }
+}
+
+#Preview("Normal View") {
+    CardImageScroller(person: Person.example, fullScreenMode: .constant(false))
+}
+
+#Preview("Full Screen") {
+    CardImageScroller(person: Person.example, fullScreenMode: .constant(true))
+}
--- TinderClone/Model/Person.swift
+++ TinderClone/Model/Person.swift
@@ -10,7 +10,7 @@ import Foundation
 struct Person: Hashable, Identifiable {
     // use real userId in real life
     let id = UUID().uuidString
-
+    
     var name: String
     var age: Int
     var bio: String
@@ -19,48 +19,119 @@ struct Person: Hashable, Identifiable {
 }
 
 extension Person {
-    static let example = Person(name: "Alex", age: 33, bio: "I am photographer and love dogs", imageURLS: [URL(string: "https://i.pravatar.cc/400?img=52")!],
-                                conversations: [
-                                    Message.exampleReceived,
-                                    Message.exampleSent,
-                                    Message(content: "Hello there. How are you doing today? How's the weather over there.", fromCurrentUser: false)]
+    static let example = Person(
+        name: "Alex",
+        age: 33,
+        bio: "I am photographer and love dogs",
+        imageURLS: [
+            URL(string: "https://i.pravatar.cc/400?img=52")!,
+            URL(string: "https://i.pravatar.cc/401")!,
+            URL(string: "https://i.pravatar.cc/402")!,
+            URL(string: "https://i.pravatar.cc/403")!
+        ],
+        conversations: [
+            Message.exampleReceived,
+            Message.exampleSent,
+            Message(content: "Hello there. How are you doing today? How's the weather over there.", fromCurrentUser: false)
+        ]
     )
-    static let example2 = Person(name: "Jessica",age: 30,  bio: "I like to go to the beach", imageURLS: [URL(string: "https://i.pravatar.cc/400?img=49")!],
-                                 conversations: [
-                                    Message.exampleReceived,
-                                    Message.exampleSent,
-                                    Message(content: "Wanna grab a coffee?", fromCurrentUser: false)]
+    static let example2 = Person(
+        name: "Jessica",
+        age: 30,
+        bio: "I like to go to the beach",
+        imageURLS: [
+            URL(string: "https://i.pravatar.cc/400?img=49")!,
+            URL(string: "https://i.pravatar.cc/401")!,
+            URL(string: "https://i.pravatar.cc/402")!,
+            URL(string: "https://i.pravatar.cc/403")!
+        ],
+        conversations: [
+            Message.exampleReceived,
+            Message.exampleSent,
+            Message(content: "Wanna grab a coffee?", fromCurrentUser: false)
+        ]
     )
-    static let example3 = Person(name: "Steven", age: 34, bio: "Nerdy boy who looks for love", imageURLS: [URL(string: "https://i.pravatar.cc/400?img=53")!],
-                                 conversations: [
-                                    Message.exampleReceived,
-                                    Message.exampleSent,
-                                    Message(content: "I am free Thursday. Where do you want to meet up?", fromCurrentUser: false)]
+    static let example3 = Person(
+        name: "Steven",
+        age: 34,
+        bio: "Nerdy boy who looks for love",
+        imageURLS: [
+            URL(string: "https://i.pravatar.cc/400?img=53")!,
+            URL(string: "https://i.pravatar.cc/401")!,
+            URL(string: "https://i.pravatar.cc/402")!,
+            URL(string: "https://i.pravatar.cc/403")!
+        ],
+        conversations: [
+            Message.exampleReceived,
+            Message.exampleSent,
+            Message(content: "I am free Thursday. Where do you want to meet up?", fromCurrentUser: false)
+        ]
     )
-    static let example4 = Person(name: "Cove", age: 26, bio: "Fashion Designer", imageURLS: [URL(string: "https://i.pravatar.cc/400?img=36")!],
-                                 conversations: [
-                                    Message.exampleReceived,
-                                    Message.exampleSent,
-                                    Message(content: "I am travelling to Australia in May. How about you?", fromCurrentUser: false)]
+    static let example4 = Person(
+        name: "Cove",
+        age: 26,
+        bio: "Fashion Designer",
+        imageURLS: [
+            URL(string: "https://i.pravatar.cc/400?img=36")!,
+            URL(string: "https://i.pravatar.cc/401")!,
+            URL(string: "https://i.pravatar.cc/402")!,
+            URL(string: "https://i.pravatar.cc/403")!
+        ],
+        conversations: [
+            Message.exampleReceived,
+            Message.exampleSent,
+            Message(content: "I am travelling to Australia in May. How about you?", fromCurrentUser: false)
+        ]
     )
-    static let example5 = Person(name: "Parker", age: 24, bio: "I love playing basketball and soccer.", imageURLS: [URL(string: "https://i.pravatar.cc/400?img=12")!],
-                                 conversations: [
-                                    Message.exampleReceived,
-                                    Message.exampleSent,
-                                    Message(content: "Sup, cutie! Would love to meet up.", fromCurrentUser: false)]
+    static let example5 = Person(
+        name: "Parker",
+        age: 24,
+        bio: "I love playing basketball and soccer.",
+        imageURLS: [
+            URL(string: "https://i.pravatar.cc/400?img=12")!,
+            URL(string: "https://i.pravatar.cc/401")!,
+            URL(string: "https://i.pravatar.cc/402")!,
+            URL(string: "https://i.pravatar.cc/403")!
+        ],
+        conversations: [
+            Message.exampleReceived,
+            Message.exampleSent,
+            Message(content: "Sup, cutie! Would love to meet up.", fromCurrentUser: false)
+        ]
     )
-    static let example6 = Person(name: "Melisa", age: 22, bio: "Savage Girl on the app", imageURLS: [URL(string: "https://i.pravatar.cc/400?img=43")!],
-                                 conversations: [
-                                    Message.exampleReceived,
-                                    Message.exampleSent,
-                                    Message(content: "On my way.", fromCurrentUser: false)]
+    static let example6 = Person(
+        name: "Melisa",
+        age: 22,
+        bio: "Savage Girl on the app",
+        imageURLS: [
+            URL(string: "https://i.pravatar.cc/400?img=43")!,
+            URL(string: "https://i.pravatar.cc/401")!,
+            URL(string: "https://i.pravatar.cc/402")!,
+            URL(string: "https://i.pravatar.cc/403")!
+        ],
+        conversations: [
+            Message.exampleReceived,
+            Message.exampleSent,
+            Message(content: "On my way.", fromCurrentUser: false)]
     )
-    static let example7 = Person(name: "Brent", age: 28, bio: "Gym rat", imageURLS: [URL(string: "https://i.pravatar.cc/400?img=14")!],
-                                 conversations: [
-                                    Message.exampleReceived,
-                                    Message.exampleSent,
-                                    Message(content: "I don't think we are a fit. Sorry", fromCurrentUser: false)]
+    static let example7 = Person(
+        name: "Brent",
+        age: 28,
+        bio: "Gym rat",
+        imageURLS: [
+            URL(string: "https://i.pravatar.cc/400?img=14")!,
+            URL(string: "https://i.pravatar.cc/401")!,
+            URL(string: "https://i.pravatar.cc/402")!,
+            URL(string: "https://i.pravatar.cc/403")!
+        ],
+        conversations: [
+            Message.exampleReceived,
+            Message.exampleSent,
+            Message(content: "I don't think we are a fit. Sorry", fromCurrentUser: false)
+        ]
     )
+    
+    
     static let allExamples: [Person] = [example, example2, example3, example4, example5, example6, example7]
     
     static func randomExamples(_ count: Int) -> [Person] {
